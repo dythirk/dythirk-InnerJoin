3cc2478e476a37ff864a5dc996964ce5a11a5a8d
Thai Vo <dythirk@gmail.com>
2024-08-26 18:16:14 +0000

    Revision

--- problem1.sql
+++ problem1.sql
@@ -1,3 +1,3 @@
-SELECT student.student_name FROM student INNER JOIN class
-ON class.class_title = student.class_title
+SELECT student.id, student.student_name FROM student INNER JOIN class
+ON student.class_title = class.class_title
 WHERE class.teacher_name = 'Ms. Lovelace';
